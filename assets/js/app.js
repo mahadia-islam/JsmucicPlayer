2564a44839db71a1c3f9deaf5c8e209e8e923c51
mahadia-islam <74854813+mahadia-islam@users.noreply.github.com>
2021-07-05 01:04:59 +0600

    all name are changed

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,36 @@
 const playButton = document.querySelector('.play-button');
 const audioMusic = document.querySelector('audio');
 const stopButton = document.querySelector('.stop-button');
+const artist = document.querySelector('.artist');
+const songName = document.querySelector('.song-name');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+// scaffolding song array
+
+const songs = [
+    {
+        name:'shayad',
+        title:'Shayad',
+        artist:'arijit singh'
+    },
+    {
+        name:'tera_hone_laga_hoon',
+        title:'tera hone laga hoon',
+        artist:'atif aslam'
+    },
+    {
+        name:'tere_liye',
+        title:'Tere Liye',
+        artist:'atif aslam'
+
+    },
+    {
+        name:'tere_liye_lyrical',
+        title:'Tere Liye',
+        artist:'atif aslam'
+    }
+]
 
 // Now Handling Event Listener
 
@@ -24,3 +54,11 @@ stopButton.addEventListener('click',() => {
 
 
 // changing music data
+
+const loadSongs = (songs) => {
+    songName.innerHTML = songs.title;
+    artist.innerHTML = songs.artist;
+    audioMusic.src = `img/${songs.name}+.mp3`
+}
+
+loadSongs(songs);
